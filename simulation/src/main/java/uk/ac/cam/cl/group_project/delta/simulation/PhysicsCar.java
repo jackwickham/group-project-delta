54912d18e92c285798be07ff15988e8c5e564bd3
jmlowenthal <jamie.lowenthal@gmail.com>
2018-02-06 23:21:52 +0000

    Add PhysicsCar specification

---
+++ simulation/src/main/java/uk/ac/cam/cl/group_project/delta/simulation/PhysicsCar.java
@@ -0,0 +1,18 @@
+package uk.ac.cam.cl.group_project.delta.simulation;
+
+/**
+ * Represents the physically simulated instantiation of a car.
+ */
+public class PhysicsCar extends PhysicsBody {
+
+	/**
+	 * Update the kinematic state of the car, considering friction forces.
+	 * @param dt    Timestep in seconds.
+	 */
+	@Override
+	public void update(double dt) throws SimulationException {
+		// TODO: implementation
+		super.update(dt);
+	}
+
+}
