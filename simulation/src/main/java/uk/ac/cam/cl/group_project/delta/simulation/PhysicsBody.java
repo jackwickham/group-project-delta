0167fe045f83f4570abcf09a00fe7fa8564ea22d
jmlowenthal <jamie.lowenthal@gmail.com>
2018-02-06 23:11:57 +0000

    Add PhysicsBody outline

---
+++ simulation/src/main/java/uk/ac/cam/cl/group_project/delta/simulation/PhysicsBody.java
@@ -0,0 +1,33 @@
+package uk.ac.cam.cl.group_project.delta.simulation;
+
+/**
+ * Represents an abstract simulated body with position, velocity and
+ * acceleration.
+ */
+public class PhysicsBody {
+
+	/**
+	 * Absolute position in world space - in metres.
+	 */
+	private Vector2D position;
+
+	/**
+	 * Vector velocity in metres per second.
+	 */
+	private Vector2D velocity;
+
+	/**
+	 * Vector acceleration in metres per second per second.
+	 */
+	private Vector2D acceleration;
+
+	/**
+	 * Solve motion equations for this body.
+	 * @param dt    Timestep in seconds.
+	 */
+	public void update(double dt) {
+		position = position.add(velocity.multiply(dt));
+		velocity = velocity.add(acceleration.multiply(dt));
+	}
+
+}
