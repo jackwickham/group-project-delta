69f0bffce360ad1a16bf2f053e6c7c9ce0c34943
jmlowenthal <jamie.lowenthal@gmail.com>
2018-02-06 15:40:47 +0000

    Add 2D vector specialisation

---
+++ src/uk/ac/cam/cl/group_project/delta/simulation/Vector2D.java
@@ -0,0 +1,15 @@
+package uk.ac.cam.cl.group_project.delta.simulation;
+
+/**
+ * Wraps a 1x2 matrix.
+ */
+public class Vector2D extends Vector {
+
+	/**
+	 * Initialise a 1x2 matrix.
+	 */
+	public Vector2D() {
+		super(2);
+	}
+	
+}
