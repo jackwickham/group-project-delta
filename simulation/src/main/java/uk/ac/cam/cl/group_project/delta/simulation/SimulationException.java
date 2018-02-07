5b1e3b75c3e9e68fbc3d72a7229ad5165a1bddcd
jmlowenthal <jamie.lowenthal@gmail.com>
2018-02-07 18:21:13 +0000

    Fill out exception constructors

--- simulation/src/main/java/uk/ac/cam/cl/group_project/delta/simulation/SimulationException.java
+++ simulation/src/main/java/uk/ac/cam/cl/group_project/delta/simulation/SimulationException.java
@@ -6,11 +6,52 @@ package uk.ac.cam.cl.group_project.delta.simulation;
 public class SimulationException extends Exception {
 
 	/**
-	 * Initialise exception with message.
-	 * @param m    Information message.
+	 * Constructs a new exception with `null` as its detail message.
 	 */
-	public SimulationException(String m) {
-		super(m);
+	public SimulationException() {
+		super();
+	}
+
+	/**
+	 * Initialise exception with message.
+	 * @param message    Information message.
+	 */
+	public SimulationException(String message) {
+		super(message);
+	}
+
+	/**
+	 * Constructs a new exception with the specified detail message and cause.
+	 * @param message    Information message.
+	 * @param cause      The cause - `null` is permitted.
+	 */
+	public SimulationException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	/**
+	 * Constructs a new exception with the specified detail message, cause,
+	 * suppression enabled or disabled, and writable stack trace enabled or
+	 * disabled.
+	 * @param message               Information message.
+	 * @param cause                 The cause - `null` is permitted.
+	 * @param enableSuppression     Allow exception suppression?
+	 * @param writableStackTrace    Should the stack trace be writable?
+	 */
+	public SimulationException(
+		String message, Throwable cause, boolean enableSuppression,
+		boolean writableStackTrace
+	) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+	/**
+	 * Constructs a new exception with the specified cause and a detail message
+	 * of `(cause == null ? null : cause.toString())`.
+	 * @param cause    The cause - `null` is permitted
+	 */
+	public SimulationException(Throwable cause) {
+		super(cause);
 	}
 
 }
