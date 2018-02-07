200d31fe32cf212b50a6d8f80bc312bea57e8368
jmlowenthal <jamie.lowenthal@gmail.com>
2018-02-07 16:43:11 +0000

    Replace overly complex hierarchy with simple 2D vector class

--- simulation/src/main/java/uk/ac/cam/cl/group_project/delta/simulation/Vector2D.java
+++ simulation/src/main/java/uk/ac/cam/cl/group_project/delta/simulation/Vector2D.java
@@ -1,15 +1,137 @@
 package uk.ac.cam.cl.group_project.delta.simulation;
 
 /**
- * Wraps a 1x2 matrix.
+ * Represents a vector of two components.
  */
-public class Vector2D extends Vector {
+public class Vector2D {
 
 	/**
-	 * Initialise a 1x2 matrix.
+	 * First vector component.
+	 */
+	private double x;
+
+	/**
+	 * Second vector component;
+	 */
+	private double y;
+
+	/**
+	 * Initialise a zero-vector.
 	 */
 	public Vector2D() {
-		super(2);
+		this.x = 0;
+		this.y = 0;
+	}
+
+	/**
+	 * Initialise vector to given values.
+	 * @param x    First component.
+	 * @param y    Second component.
+	 */
+	public Vector2D(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	/**
+	 * Fetch x component.
+	 * @return    First vector component.
+	 */
+	public double getX() {
+		return this.x;
+	}
+
+	/**
+	 * Fetch y component.
+	 * @return    Second vector component.
+	 */
+	public double getY() {
+		return this.y;
+	}
+
+	/**
+	 * Sum the two vectors, piecewise.
+	 * @param a    Left-hand side vector.
+	 * @param b    Right-hand side vector.
+	 * @return     2D vector with components equal to the sum of the inputs.
+	 */
+	public static Vector2D add(Vector2D a, Vector2D b) {
+		return new Vector2D(
+			a.getX() + b.getX(),
+			a.getY() + b.getY()
+		);
+	}
+
+	/**
+	 * Add this vector to another returning a new vector as the result.
+	 * @param other    Vector to add.
+	 * @return         Sum of this vector and the other.
+	 */
+	public Vector2D add(Vector2D other) {
+		return Vector2D.add(this, other);
+	}
+
+	/**
+	 * Multiply both vector components by given scale to produce new vector.
+	 * @param vec      Vector to scale.
+	 * @param scale    Scale factor.
+	 * @return         Resulting vector.
+	 */
+	public static Vector2D multiply(Vector2D vec, double scale) {
+		return new Vector2D(
+			vec.getX() * scale,
+			vec.getY() * scale
+		);
+	}
+
+	/**
+	 * Multiply both vector components by given scale to produce new vector.
+	 * @param scale    Scale factor.
+	 * @return         Scaled vector.
+	 */
+	public Vector2D multiply(double scale) {
+		return Vector2D.multiply(this, scale);
+	}
+
+	/**
+	 * Computes the dot product of the input vectors, that is for inputs
+	 * (x1, y1) and (x2, y2) the output is (x1 * x2 + y1 * y2).
+	 * @param a    Left-hand side vector.
+	 * @param b    Right-hand side vector.
+	 * @return     Dot product result.
+	 */
+	public static double dot(Vector2D a, Vector2D b) {
+		return a.getX() * b.getX() + a.getY() * b.getY();
+	}
+
+	/**
+	 * Computes the dot product of the input vectors, that is for inputs
+	 * (x1, y1) and (x2, y2) the output is (x1 * x2 + y1 * y2).
+	 * @param other    Vector to dot with.
+	 * @return         Dot product result.
+	 */
+	public double dot(Vector2D other) {
+		return Vector2D.dot(this, other);
+	}
+
+	/**
+	 * Computes a normal vector to the provided argument. In this case, the
+	 * vector returned has undergone a 90 degree clockwise rotation in the
+	 * XY-plane.
+	 * @param vec    Initial vector.
+	 * @return       Orthogonal vector.
+	 */
+	public static Vector2D normal(Vector2D vec) {
+		return new Vector2D(vec.getY(), -vec.getX());
+	}
+
+	/**
+	 * Computes a normal vector to this vector. In this case, the vector
+	 * returned has undergone a 90 degree clockwise rotation in the XY-plane.
+	 * @return    Orthogonal vector.
+	 */
+	public Vector2D normal() {
+		return Vector2D.normal(this);
 	}
 
 }
