92556fd80b3c47c09cee3ae214efea65aca474c1
Jack Wickham <jackwickham@live.co.uk>
2018-02-13 15:32:11 +0000

    Log test results and depend on mockito

---
+++ lego/src/test/java/uk/ac/cam/cl/group_project/delta/lego/BeaconTrackerTest.java
@@ -0,0 +1,22 @@
+package uk.ac.cam.cl.group_project.delta.lego;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import uk.ac.cam.cl.group_project.delta.Beacon;
+
+import static org.junit.Assert.*;
+
+public class BeaconTest {
+	private Beacon classUnderTest;
+
+	@Before
+	public void createBeaconInstance() {
+		Mockito.mock(EV3)
+	}
+
+	@Test
+	public void testCorrectValueOnOne() {
+
+	}
+}
