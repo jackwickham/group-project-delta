ad587dc17422f51c124e1d546c21c4495a58c4f0
AARON-ASUS\Aaron <aaron@the-huttons.co.uk>
2018-02-15 11:27:58 +0000

    Fixed formatting, moved constant functions into Algorithm class

--- common/src/main/java/uk/ac/cam/cl/group_project/delta/algorithm/communications/MessageType.java
+++ common/src/main/java/uk/ac/cam/cl/group_project/delta/algorithm/communications/MessageType.java
@@ -4,41 +4,37 @@ import java.util.HashMap;
 import java.util.Map;
 
 public enum MessageType {
-	Emergency(0),
-	Data(1),
-	RequestToMerge(2),
-	AcceptToMerge(3),
-	ConfirmMerge(4),
-	MergeComplete(5);
-	
+	Emergency(0), Data(1), RequestToMerge(2), AcceptToMerge(3), ConfirmMerge(4), MergeComplete(5);
+
 	private int value;
 	private static Map<Integer, MessageType> lookup = new HashMap<>();
-	
+
 	private MessageType(int value) {
 		this.value = value;
 	}
-	
+
 	/**
-	 * Create the mapping from integer values to MessageTypes.
-	 * Slightly hacky but not much choice
+	 * Create the mapping from integer values to MessageTypes. Slightly hacky but
+	 * not much choice
 	 */
 	static {
-        for (MessageType type : MessageType.values()) {
-            lookup.put(type.value, type);
-        }
-    }
+		for (MessageType type : MessageType.values()) {
+			lookup.put(type.value, type);
+		}
+	}
 
 	/**
 	 * Lookup which MessageType a particular value corresponds to.
 	 * 
-	 * @param value - the value of the MessageType to be returned
+	 * @param value
+	 *            - the value of the MessageType to be returned
 	 * @return the associated MessageType
 	 */
-    public static MessageType valueOf(int value) {
-        return lookup.get(value);
-    }
-    
-    public int getValue() {
-    	return value;
-    }
+	public static MessageType valueOf(int value) {
+		return lookup.get(value);
+	}
+
+	public int getValue() {
+		return value;
+	}
 }
