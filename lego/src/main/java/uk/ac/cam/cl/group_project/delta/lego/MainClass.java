ab7ad75f19179403199f78df2680595dc8e12a98
Aliyah Bond <ajbondmk@hotmail.co.uk>
2018-02-17 14:22:27 +0000

    Tidy up main class

--- lego/src/main/java/uk/ac/cam/cl/group_project/delta/lego/MainClass.java
+++ lego/src/main/java/uk/ac/cam/cl/group_project/delta/lego/MainClass.java
@@ -1,7 +1,6 @@
 package uk.ac.cam.cl.group_project.delta.lego;
 
 import lejos.hardware.BrickFinder;
-import lejos.hardware.Keys;
 import lejos.hardware.ev3.EV3;
 import lejos.hardware.lcd.TextLCD;
 
@@ -9,12 +8,7 @@ class MainClass {
 	public static void main(String[] args) {
 		EV3 ev3 = (EV3) BrickFinder.getLocal();
 		TextLCD lcd = ev3.getTextLCD();
-		Keys keys = ev3.getKeys();
-
+		//Keys keys = ev3.getKeys();
 		lcd.drawString("This is " + ev3.getName(), 0, 4);
-		Drive d = new Drive(ev3);
-		keys.waitForAnyPress();
-		d.stop();
-
 	}
 }
