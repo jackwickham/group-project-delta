774efe0bfb53fd8c672953cdd29e15778cfe2e65
Aliyah Bond <ajbondmk@hotmail.co.uk>
2018-02-17 14:23:50 +0000

    Reset main class to master

--- lego/src/main/java/uk/ac/cam/cl/group_project/delta/lego/MainClass.java
+++ lego/src/main/java/uk/ac/cam/cl/group_project/delta/lego/MainClass.java
@@ -1,6 +1,7 @@
 package uk.ac.cam.cl.group_project.delta.lego;
 
 import lejos.hardware.BrickFinder;
+import lejos.hardware.Keys;
 import lejos.hardware.ev3.EV3;
 import lejos.hardware.lcd.TextLCD;
 
@@ -8,7 +9,9 @@ class MainClass {
 	public static void main(String[] args) {
 		EV3 ev3 = (EV3) BrickFinder.getLocal();
 		TextLCD lcd = ev3.getTextLCD();
-		//Keys keys = ev3.getKeys();
+		Keys keys = ev3.getKeys();
+
 		lcd.drawString("This is " + ev3.getName(), 0, 4);
+		keys.waitForAnyPress();
 	}
 }
