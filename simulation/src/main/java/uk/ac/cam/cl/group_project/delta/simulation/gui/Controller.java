e15cae2cd4cb5a0e608674c9415e5fae2e5d2583
jmlowenthal <jamie.lowenthal@gmail.com>
2018-02-19 17:50:37 +0000

    Add click events to display tree properties view

--- simulation/src/main/java/uk/ac/cam/cl/group_project/delta/simulation/gui/Controller.java
+++ simulation/src/main/java/uk/ac/cam/cl/group_project/delta/simulation/gui/Controller.java
@@ -1,9 +1,15 @@
 package uk.ac.cam.cl.group_project.delta.simulation.gui;
 
 import javafx.event.Event;
-import javafx.event.EventTarget;
 import javafx.fxml.FXML;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import uk.ac.cam.cl.group_project.delta.simulation.SimulatedCar;
+import uk.ac.cam.cl.group_project.delta.simulation.Vector2D;
+
+import java.util.Random;
 
 public class Controller {
 
@@ -14,6 +20,9 @@ public class Controller {
 
 	@FXML
 	private Pane viewPane;
+
+	@FXML
+	private TreeView<String> propertiesView;
 
 	/**
 	 * Construct the application's simulation thread.
@@ -28,6 +37,44 @@ public class Controller {
 	@FXML
 	public void initialize() {
 		simulation.start();
+	}
+
+	private void showProperties(Treeable obj) {
+		TreeItem<String> root = obj.toTree();
+		root.setExpanded(true);
+		propertiesView.setRoot(root);
+	}
+
+	@FXML
+	public void createNewSimulationObject(Event event) {
+
+		SimulatedCar car = simulation.createCar(0.15);
+
+		Random random = new Random();
+		car.setPosition(
+			new Vector2D(
+				random.nextDouble() * 1000,
+				random.nextDouble() * 1000
+			)
+		);
+		car.setVelocity(
+			new Vector2D(
+				random.nextDouble() * 1000,
+				random.nextDouble() * 1000
+			)
+		);
+
+		SimulatedBodyNode node = new SimulatedBodyNode(car);
+		node.addEventFilter(
+			MouseEvent.MOUSE_CLICKED,
+			e -> {
+				showProperties(node);
+				e.consume();
+			}
+		);
+
+		viewPane.getChildren().add(node);
+
 	}
 
 }
---
+++ simulation/src/main/java/uk/ac/cam/cl/group_project/delta/simulation/gui/Treeable.java
@@ -0,0 +1,9 @@
+package uk.ac.cam.cl.group_project.delta.simulation.gui;
+
+import javafx.scene.control.TreeItem;
+
+public interface Treeable {
+
+	public TreeItem<String> toTree();
+
+}
