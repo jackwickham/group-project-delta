d749f54f13bb13babff213f15b66d15e504755b6
jmlowenthal <jamie.lowenthal@gmail.com>
2018-02-19 17:51:07 +0000

    Add PhysicsBody wrapper

---
+++ simulation/src/main/java/uk/ac/cam/cl/group_project/delta/simulation/gui/SimulatedBodyNode.java
@@ -0,0 +1,57 @@
+package uk.ac.cam.cl.group_project.delta.simulation.gui;
+
+import javafx.scene.Group;
+import javafx.scene.control.TreeItem;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import uk.ac.cam.cl.group_project.delta.simulation.PhysicsBody;
+
+public class SimulatedBodyNode extends Group implements Treeable {
+
+	private final PhysicsBody body;
+
+	public SimulatedBodyNode(PhysicsBody body) {
+
+		this.body = body;
+
+		Circle c = new Circle(10.0);
+		c.setFill(Color.TRANSPARENT);
+		c.setStroke(Color.BLACK);
+		getChildren().add(c);
+
+		/*this.translateXProperty().bind(new DoubleBinding() {
+			@Override
+			protected double computeValue() {
+				return body.getPosition().getX();
+			}
+		});
+
+		this.translateYProperty().bind(new DoubleBinding() {
+			@Override
+			protected double computeValue() {
+				return body.getPosition().getY();
+			}
+		});*/
+
+	}
+
+	public PhysicsBody getBody() {
+		return body;
+	}
+
+	public TreeItem<String> toTree() {
+		TreeItem<String> root = new TreeItem<>("Body #" + body.getUuid());
+
+		TreeItem<String> position = new TreeItem<>("position");
+		root.getChildren().add(position);
+		position.getChildren().add(
+			new TreeItem<>("x = " + body.getPosition().getX())
+		);
+		position.getChildren().add(
+			new TreeItem<>("y = " + body.getPosition().getY())
+		);
+
+		return root;
+	}
+
+}
