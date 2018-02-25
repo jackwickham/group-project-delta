a96fae94370d9c01c3dda9f2a6dbe636766d272a
Aliyah Bond <ajbondmk@hotmail.co.uk>
2018-02-25 13:58:38 +0000

    Create algorithm and run it

--- lego/src/main/java/uk/ac/cam/cl/group_project/delta/lego/MainClass.java
+++ lego/src/main/java/uk/ac/cam/cl/group_project/delta/lego/MainClass.java
@@ -4,6 +4,10 @@ import lejos.hardware.BrickFinder;
 import lejos.hardware.Keys;
 import lejos.hardware.ev3.EV3;
 import lejos.hardware.lcd.TextLCD;
+import uk.ac.cam.cl.group_project.delta.algorithm.Algorithm;
+import uk.ac.cam.cl.group_project.delta.algorithm.AlgorithmEnum;
+
+import java.io.IOException;
 
 class MainClass {
 	public static void main(String[] args) {
@@ -11,10 +15,23 @@ class MainClass {
 		TextLCD lcd = ev3.getTextLCD();
 		Keys keys = ev3.getKeys();
 
+		lcd.drawString("This is " + ev3.getName(), 0, 4);
+
 		Drive drive = new Drive(ev3);
 		Sensor sensor = new Sensor(drive, ev3);
+		Network network = null;
+		try {
+			network = new Network(Thread.currentThread());
+			Algorithm algo = Algorithm.createAlgorithm(AlgorithmEnum.BasicAlgorithm, drive, sensor, network);
+			algo.initialise();
+			algo.run();
+		} catch (IOException e) {
+			lcd.drawString("IOException in network", 0, 5);
+		} finally {
+			if (network != null) {
+				network.close();
+			}
+		}
 
-		lcd.drawString("This is " + ev3.getName(), 0, 4);
-		keys.waitForAnyPress();
 	}
 }
