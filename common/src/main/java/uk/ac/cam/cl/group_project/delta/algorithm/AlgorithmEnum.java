e3c582f6edc802ab90bdda0a25cca488debfb7b4
Jack Wickham <jackwickham@live.co.uk>
2018-03-04 15:05:01 +0000

    Add spaces to algorithm display names

--- common/src/main/java/uk/ac/cam/cl/group_project/delta/algorithm/AlgorithmEnum.java
+++ common/src/main/java/uk/ac/cam/cl/group_project/delta/algorithm/AlgorithmEnum.java
@@ -1,5 +1,10 @@
 package uk.ac.cam.cl.group_project.delta.algorithm;
 
 public enum AlgorithmEnum {
-	Naive1, Naive2, Naive3, Adaptive_Cruise_Control, Cooperative_Adaptive_Cruise_Control
+	Naive1, Naive2, Naive3, Adaptive_Cruise_Control, Cooperative_Adaptive_Cruise_Control;
+
+	@Override
+	public String toString() {
+		return super.toString().replace('_', ' ');
+	}
 }
